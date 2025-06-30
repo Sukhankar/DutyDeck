fde65bc93e0b85b9f33979793adf1960e41ca390
SubhanKhalif <subhankhalif34@gmail.com>
2025-06-30 18:06:33 +0530

    added mentor login and forget passwordlogic with node mailer

--- task-management-system/src/api.js
+++ task-management-system/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:"http://localhost:5000/api", //import.meta.env.VITE_API_BASE_URL ||
+  baseURL:import.meta.env.VITE_API_BASE_URL ||"http://localhost:5000/api", //import.meta.env.VITE_API_BASE_URL ||
 });
 
 export default API;
